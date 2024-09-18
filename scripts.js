7471544dec9040d236fa1252adcec0a887b812aa
Verneloira <fernandaverne.fvds31@gmail.com>
2024-09-18 14:55:54 -0300

    Atualizaçao

--- scripts.js
+++ scripts.js
@@ -1,37 +1,51 @@
 const convertButton = document.querySelector(".convert-button")
-const currencySelect = document.querySelector(".currency-select")
-
+console.log()
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
 
-   console.log(currencySelect.value)
+    
     const dolarToday = 5.2
     const euroToday = 6.2
-} 
-    if(currencySelect.value == "dolar"){
+
     
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(inputCurrencyValue / dolarToday)
-}
-
-if(currencySelect.value == "euro"){
-   currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-    style: "currency",
-    currency: "EUR"
-
-}).format(inputCurrencyValue / euroToday)
-}
+    if(currencySelec.value == "dolar"){
+         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+             style: "currency",
+             currency: "USD"
+        }).format(inputcurrencyValue/dolarToday)
+    }
+    if(currencySelec.value == "euro"){
+         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+             style: "currency",
+             currency: "USD"
+        }).format(inputcurrencyValue/euroToday)
+    }
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
+    }).format(inputcurrencyValue)
+
+
+
+
+}
+
+function changeCurrincy(){
+    const currencyName = document.getElementById("currency-name")
+    const currencyimage = document.querySelector(".currency-img")
+    
+    if(currencySelec.value == "dolar"){
+    currencyName.innerHTML = "Dólar americano"
+    currencyimage.src =./assets/dolar.png
+    }
    
-    }).format(convertedValue)
-
-
-
-convertButton.addEventListener("click", convertValues)
+    if(currencySelec.value == "euro"){
+    currencyName.innerHTML = "Euro"
+    currencyimage.src  =./assets/euro.png
+   }
+}
+currencySelect.addEventListener("change")
+convertButton.addEventListener("click", convertValues)
